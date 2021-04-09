bb98f7458eae73068bc9f4161b009fa4f3310dd3
AdhyyanNarang <adhyyan-n@berkeley.edu>
2021-04-08 18:27:40 -0700

    I think it might be working

--- dd_game.py
+++ dd_game.py
@@ -55,7 +55,7 @@ class DecisionDependentGame(object):
         cov_x_p2, sigma_y_p2, beta_p2, mu_p2, gamma_p2 = self.p2_data_params
         theta_PO_p1, theta_PO_p2 = solve_theta_PO(mu_p1, mu_p2, gamma_p1, gamma_p2, beta_p1, beta_p2, cov_x_p1, cov_x_p2)
         return theta_PO_p1, theta_PO_p2
-    
+
     def solve_social_opt(self):
         cov_x_p1, sigma_y_p1, beta_p1, mu_p1, gamma_p1 = self.p1_data_params
         cov_x_p2, sigma_y_p2, beta_p2, mu_p2, gamma_p2 = self.p2_data_params
--- utils/utils_functions.py
+++ utils/utils_functions.py
@@ -4,25 +4,6 @@ import ipdb
 """
 Data
 """
-def obtain_data(n, d_1, d_2, mu_1, gamma_1, mu_2, gamma_2,
-                cov_x_1, cov_x_2, sigma_y_1, sigma_y_2,
-                beta_1, beta_2):
-
-    theta_1_lst, theta_2_lst = [], []
-    z_1_lst, z_2_lst = [], []
-
-    for i in np.arange(2*n):
-        # Both players play random actions to gather information
-        theta_1_i, theta_2_i = np.random.normal(size=d_1), np.random.normal(size=d_2)
-        theta_1_lst.append(theta_1_i)
-        theta_2_lst.append(theta_2_i)
-
-        z_1_i = sample_from_distribution(cov_x_1, sigma_y_1, beta_1, mu_1, gamma_1, theta_1_i, theta_2_i)
-        z_2_i = sample_from_distribution(cov_x_2, sigma_y_2, beta_2, mu_2, gamma_2, theta_1_i, theta_2_i)
-        z_1_lst.append(z_1_i)
-        z_2_lst.append(z_2_i)
-    return np.array(z_1_lst), np.array(z_2_lst), np.array(theta_1_lst), np.array(theta_2_lst)
-
 def sample_from_location_family(cov_x, sigma_y, beta, mu, gamma, theta_me, theta_other):
     x = np.random.multivariate_normal(np.zeros(len(cov_x)), cov_x)
     U_y = np.random.normal(0, sigma_y)
@@ -49,18 +30,18 @@ def solve_theta_PO(mu_1, mu_2, gamma_1, gamma_2, beta_1, beta_2, Sigma_1, Sigma_
     """
     d_1 = np.size(mu_1)
     d_2 = np.size(mu_2)
-    
+
     mu_Sig_1 = np.linalg.inv(np.outer(mu_1, mu_1) + Sigma_1)
     mu_Sig_2 = np.linalg.inv(np.outer(mu_2, mu_2) + Sigma_2)
-    mu_gamma_1 = np.outer(mu_1, gamma_1) 
+    mu_gamma_1 = np.outer(mu_1, gamma_1)
     mu_gamma_2 = np.outer(mu_2, gamma_2)
-    
+
     A_1 = np.linalg.inv(np.eye(d_1) - mu_gamma_1 @ mu_Sig_2 @ mu_gamma_2)
     A_2 = np.linalg.inv(np.eye(d_2) - mu_gamma_2 @ mu_Sig_1 @ mu_gamma_1)
-    
+
     B_1 = mu_Sig_1 @ (Sigma_1@beta_1 - mu_gamma_1@mu_Sig_2@Sigma_2@beta_2)
     B_2 = mu_Sig_2 @ (Sigma_2@beta_2 - mu_gamma_2@mu_Sig_1@Sigma_1@beta_1)
-    
+
     theta_PO_1 = A_1 @ B_1
     theta_PO_2 = A_2 @ B_2
     return theta_PO_1, theta_PO_2
@@ -71,23 +52,23 @@ def solve_theta_SO(mu_1, mu_2, gamma_1, gamma_2, beta_1, beta_2, Sigma_1, Sigma_
     """
     d_1 = np.size(mu_1)
     d_2 = np.size(mu_2)
-    
+
     mm_1 = np.outer(mu_1,mu_1)
     mm_2 = np.outer(mu_2,mu_2)
     gg_1 = np.outer(gamma_1,gamma_1)
     gg_2 = np.outer(gamma_2,gamma_2)
     mg_1 = np.outer(mu_1,gamma_1)
     mg_2 = np.outer(mu_2,gamma_2)
-    
+
     mgSig_1 = np.linalg.inv(Sigma_1 + mm_1 + gg_2)
     mgSig_2 = np.linalg.inv(Sigma_2 + gg_1 + mm_2)
-    
+
     A_1 = np.linalg.inv(np.eye(d_1) + (mg_1 + mg_2) @ mgSig_2 @ (Sigma_2 @ beta_2 - (mg_1 + mg_2)))
     A_2 = np.linalg.inv(np.eye(d_2) + (mg_2 + mg_1) @ mgSig_1 @ (Sigma_1 @ beta_1 - (mg_2 + mg_1)))
-    
+
     B_1 = mgSig_1 @ (Sigma_1 @ beta_1)
     B_2 = mgSig_2 @ (Sigma_2 @ beta_2)
-    
+
     theta_SO_1 = A_1 @ B_1
     theta_SO_2 = A_2 @ B_2
     return theta_SO_1, theta_SO_2
@@ -106,13 +87,13 @@ def solve_distribution_params(z_lst, theta_me_lst, theta_other_lst):
     gamma_hat = mu_tilde[num_each:]
     return mu_hat, gamma_hat
 
-def find_qs(mu_hat, gamma_hat, z_lst, theta_1_lst, theta_2_lst):
+def find_qs(mu_hat, gamma_hat, z_lst, theta_me_lst, theta_other_lst):
     y_lst = [e[1] for e in z_lst]
     q_lst = []
     for (idx, y) in enumerate(y_lst):
-        theta_1 = theta_1_lst[idx]
-        theta_2 = theta_2_lst[idx]
-        q = y - np.dot(mu_hat, theta_1) - np.dot(gamma_hat, theta_2)
+        theta_me = theta_me_lst[idx]
+        theta_other = theta_other_lst[idx]
+        q = y - np.dot(mu_hat, theta_me) - np.dot(gamma_hat, theta_other)
         q_lst.append(q)
     return np.array(q_lst)
 
