15580754dbfc6ef918d035b42302726d328a7145
Evan Faulkner <evanjfaulkner@gmail.com>
2021-09-22 16:50:33 -0700

    dfo variable batch

--- Uber Lyft Simulation/dfo_player_rideshare.py
+++ Uber Lyft Simulation/dfo_player_rideshare.py
@@ -34,7 +34,7 @@ class DFOPlayer(object):
         return theta_init
     
     def perturb_theta(self):
-        u = sample_sphere(self.delta/np.log10((len(self.theta_history)+2)),
+        u = sample_sphere(self.delta*np.log((len(self.theta_history)+2))/(len(self.theta_history)+2),
                           len(self.theta_history[-1]))
         self.u_history.append(u)
         return self.theta_history[-1]+u
@@ -42,7 +42,8 @@ class DFOPlayer(object):
     def update_theta(self,oracle_risk):
         self.risk_history.append(oracle_risk)
         batch_size = len(oracle_risk)
-        theta_new = self.theta_history[-1]-(self.eta*np.dot(np.array(self.u_history[-batch_size:]).reshape(-1,batch_size), np.array(oracle_risk).reshape((batch_size,1)))/batch_size)
+        theta_new = self.theta_history[-1]-(self.eta/np.log(np.log(len(self.theta_history)+2)+2)*
+                                            np.dot(np.array(self.u_history[-batch_size:]).reshape(-1,batch_size), np.array(oracle_risk).reshape((batch_size,1)))/batch_size)
 #         theta_new = self.theta_history[-1]-((self.eta/np.log10((len(self.theta_history)/10+2))*oracle_risk*self.u_history[-1]))
 #         print(f'theta_old = {self.theta_history[-1]}')
 #         print(f'theta_new = {theta_new}')
